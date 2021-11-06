fcd67036ead53dd98f904615847d26bdcfb1c2b9
Carlos Andres Pasten Bravo <carlos92.pastenbravo@gmail.com>
2021-11-06 16:53:58 -0300

    routes issues do it

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import CharacterInformation from '../containers/Character-Information';
 import NotFound from '../containers/NotFound';
+import Aboutus from '../containers/AboutUs';
 import Layout from '../components/Layout';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         { /* in the version 6 of react-router-dom, the Routes component is deprecated and replaced by Routes 
-           and the atribute component is deprecated and replaced by element */}
-        <Routes>
-          <Route exact path="/" element={CharacterInformation} />
-          <Route element={NotFound} />
-        </Routes>
+           and the atribute component is deprecated and replaced by element */ }
+        <Switch>
+          <Route exact path="/" component={CharacterInformation} />
+          <Route exact path='/about' component={Aboutus}/>
+          <Route path="*" component={NotFound} />
+        </Switch>
       </Layout>
     </BrowserRouter>
   ); 
